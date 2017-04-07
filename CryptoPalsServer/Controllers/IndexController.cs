292937e2e38b87cd4cebb8b2639196fe6faa9799
Mattias Beimers <mattias.beimers@topicus.nl>
2017-04-07 17:56:33 +0200

    Add a cryptopals webapplication

---
+++ CryptoPalsServer/Controllers/IndexController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoPalsServer.Controllers
+{
+    [Route("")]
+    public class IndexController : Controller
+    {
+        [HttpGet]
+        public string Get(string file, string signature) {
+            return "Cryptopals API";
+        }
+    }
+}
---
+++ CryptoPalsServer/Controllers/Set4/Challenge31Controller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    public class Challenge31Controller : Controller
+    {
+        [HttpGet]
+        public string Get(string file, string signature) {
+            return $"file: {file}, signature: {signature}";
+        }
+    }
+}
